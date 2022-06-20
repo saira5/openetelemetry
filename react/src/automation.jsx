b10df9c7b6cc2782aa09213574f1abdd43039ca0
saira <saira.mehmood@hotmail.com>
2022-06-20 20:09:37 +0500

    code cleanup

--- react/src/automation.jsx
+++ react/src/automation.jsx
@@ -5,10 +5,6 @@ import api from "@opentelemetry/api";
 import React from 'react';
 import opentelemetry from "@opentelemetry/api";
 const _parentSpanMap = new Map();
-/**Steps to follow
- * start the span 
- * set te parent space by setting the actice context 
- * */
 const self = this;
 export const UserContext = React.createContext();
 
@@ -16,84 +12,6 @@ export const UserContext = React.createContext();
 const originalUseEffect = React.useEffect;
 const originalSetState = React.setState;
 const react=React.Component;
-
-// export const _instrumentFunction = (span, original) => {
-
-//     let wasError = false;
-//     try {
-//         return api.context.with(api.trace.setSpan(api.context.active(), span), () => {
-//             return original;
-//         });
-//     }
-//     catch (err) {
-//         //span.setAttribute(AttributeNames.REACT_ERROR, err.stack);
-//         wasError = true;
-//         throw err;
-//     }
-//     finally {
-//       //  span.end();
-
-//     }
-// }
-
-/**
-     * Patches the render lifecycle method
-     */
-export const _patchUseEffect = () => {
-    // return (original) => {
-    const plugin = this;
-    // return function patchUseEffect(...args) {
-
-    //setting the span 
-    // const tracer = getTracing("example-react-load", AttributeNames.EFFECT_SPAN);
-    // tracer.end();
-    // const apply = plugin._instrumentFunction(this, 'componentDidMount', tracer, () => {
-    //     return original.apply(this, args);
-    // });
-
-    // return original.apply(this, args);        
-    //  };
-    // };
-}
-
-
-
-/**
-     * Patches the setState function
-     */
-export const _patchSetState = () => {
-    return (original) => {
-        const plugin = this;
-        return function patchSetState(...args) {
-            const tracer = getTracing("example-react-load", AttributeNames.EFFECT_SPAN);
-            tracer.end();
-
-            //     const parentSpan = plugin._getParentSpan(this, AttributeNames_1.AttributeNames.UPDATING_SPAN);
-            return plugin._instrumentFunction(this, 'setState()', tracer, () => {
-                return original.apply(this, args);
-            });
-        };
-    };
-}
-
-/**
-    * patch function which wraps all the lifecycle methods
-    */
-export const patch = () => {
-    console.log("Patch function called")
-    console.log("context", UserContext)
-
-    // // Make a copy of the original add function 
-    // const origUseEffect = React.useEffect;
-    // React.useEffect = function() {
-    //     //console.log(`Adding the result of ${a} and ${b}`);
-    //     return origUseEffect();
-    // }
-    shimmer.wrap(React.useEffect, 'useEffect', _patchUseEffect());
-    //  shimmer.wrap(this, 'setState',_patchSetState());
-}
-
-
 
 export const useEffectMonkeyPatching = () => {
     //  Make a copy of the original add function 
@@ -114,9 +32,6 @@ export const useEffectMonkeyPatching = () => {
             }
           );
       return apply;
-        // const tracer = getTracing("example-react-load", AttributeNames.EFFECT_SPAN);
-        // tracer.end();
-      //  return originalUseEffect.apply(React, args);
     }
 
 }
@@ -133,15 +48,6 @@ export const setStateMonkeyPatching = () => {
         return originalSetState.apply(React, args);
     }
 
-}
-
-
-/**
-    * unpatch function to unwrap all the lifecycle methods
-    */
-export const unpatch = () => {
-    console.log("Unpatch method called")
-    shimmer.unwrap(this, 'useEffect');
 }
 
 /**
